6e6db8f0272ba5c66477a5b578e56fae88e41bc2
Tibor den Held <tibordenheld@Tibors-MacBook-Pro.local>
2018-10-11 16:17:17 +0200

    #times - refactored from .hbs to liquid

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,7 +60,6 @@ $(document).ready(function () {
             element: $($.attr(this, "href")),
             offst: $(".header").height(),
         };
-        console.log("test");
         TweenMax.to(window, .8, {
             ease: Power3.easeInOut,
             scrollTo: {
@@ -71,6 +70,101 @@ $(document).ready(function () {
         });
         return false;
     });
+
+
+
+    // toggle modalities
+    // ------------------------------------------------------------------
+
+    function modalities() {
+        var modalities = [
+            "train",
+            "metro",
+            "tram",
+            "bus",
+            "ferry",
+        ];
+
+        var default_state = modalities[0];
+
+        function toggle_modalities(b, t) {
+            // hide all tables
+            $(".js-tm-table").hide();
+
+            // show default
+            $(".js-tm-table__" + default_state).show();
+            $(".js-tm__" + default_state).addClass("is-active");
+
+            $(b).click(function () {
+                // transition to slide out the current cols of the table
+                TweenMax.set(".js-times-col", {
+                    x: -75,
+                    autoAlpha: 0
+                });
+
+                // set delay to let the transition fade out the current state
+                setTimeout(function () {
+                    // slide in new cols
+                    TweenMax.fromTo(".js-times-col", 0.001, {
+                        x: 75,
+                        autoAlpha: 0
+                    }, {
+                        x: 0,
+                        autoAlpha: 1
+                    });
+
+                    // hide all tables and show the one that's clicked
+                    $(".js-tm-table").hide();
+                    $(t).show();
+                }, 300);
+
+                // remove all active states of the toggle
+                $(".js-tm").removeClass("is-active");
+
+                // add the one that"s clicked
+                $(b).addClass("is-active");
+            });
+        };
+
+        for (i = 0; i < modalities.length; i++) {
+            toggle_modalities(
+                ".js-tm__" + modalities[i],
+                ".js-tm-table__" + modalities[i]
+            );
+        };
+    };
+
+    // init function
+    modalities();
+
+
+
+
+
+    // clock
+    // ------------------------------------------------------------------
+
+    function init_local_clocks() {
+        // Get the local time using JS
+        var date = new Date;
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+
+        var angle_hours = (hours * 30) + (minutes / 2);
+        var angle_minutes = (minutes * 6);
+        var angle_seconds = (seconds * 6);
+
+        $(".clock__hours").css({
+            "transform": "rotateZ(" + angle_hours + "deg)"
+        });
+        $(".clock__minutes").css({
+            "transform": "rotateZ(" + angle_minutes + "deg)"
+        });
+        //$(".clock__seconds").css({"transform":"rotateZ("+angle_seconds+"deg)"});
+    };
+
+    init_local_clocks();
 
 
 
